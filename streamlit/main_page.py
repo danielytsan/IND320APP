93d956af43785e72bea1eb0cdd1402c2110f57e5
danielytsan <danielyangtsan@gmail.com>
2026-09-25 13:44:38 +0200

    starting on page 1

--- streamlit/main_page.py
+++ streamlit/main_page.py
@@ -3,3 +3,6 @@ import streamlit as st
 st.set_page_config(page_title = "Main page", initial_sidebar_state = "expanded")
 
 st.title("Data")
+st.write("Welcome to the main page of my streamlit app" \
+"on the left side you can navigate between the pages" \
+"I hope you enjoy the app and find it useful")
---
+++ streamlit/pages/page1.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(page_title = "Rescervoirs Data", initial_sidebar_state = "expanded")
+df = pd.read_csv("../data/reservoirs.csv")
+st.dataframe(df)
