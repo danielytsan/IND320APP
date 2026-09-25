9b5b9f67231a44963b7d3f5c079f7c85150e0301
danielytsan <danielyangtsan@gmail.com>
2026-09-25 13:45:09 +0200

    small fix

--- streamlit/pages/page1.py
+++ streamlit/pages/page1.py
@@ -2,5 +2,5 @@ import streamlit as st
 import pandas as pd
 
 st.set_page_config(page_title = "Rescervoirs Data", initial_sidebar_state = "expanded")
-df = pd.read_csv("../data/reservoirs.csv")
+df = pd.read_csv("data/reservoirs.csv")
 st.dataframe(df)
